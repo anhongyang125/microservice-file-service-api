8d875bb28e7122d1921ad543801d6e48897ec467
zhiyong.yang <zhiyong.yang@ubtrobot.com>
2017-05-31 10:58:45 +0800

    初始化代码

---
+++ src/main/java/com/ubtechinc/service/FileService.java
@@ -0,0 +1,14 @@
+package com.ubtechinc.service;
+
+/**
+ * @title 
+ *
+ * @author Young
+ *
+ * @date 2017年5月31日上午11:00:41
+ *
+ * Copyright (C)2012-2017 深圳优必选科技 All rights reserved.
+ */
+public interface FileService {
+
+}
